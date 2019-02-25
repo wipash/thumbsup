50f1b0b5f1f392ba0b45f7080924414a49f639a5
Sean McGrath <sean@mcgrath.net.nz>
2019-02-25 14:28:49 -0500

    Escape all characters in file and folder names

--- src/website/theme-base/helpers/relative.js
+++ src/website/theme-base/helpers/relative.js
@@ -3,6 +3,12 @@ const path = require('path')
 module.exports = (target, options) => {
   const albumPath = options.data.root.album.path
   const relative = path.relative(path.dirname(albumPath), target)
-  // Escape single/double quotes
-  return relative.replace(/'/g, '%27').replace(/"/g, '%22')
+
+  // Escape all characters
+  let splitUrl = relative.split('/');
+  splitUrl.forEach((element, index, array) => {
+    array[index] = encodeURIComponent(array[index]);
+  });
+  return splitUrl.join('/');
+
 }
